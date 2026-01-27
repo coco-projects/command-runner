adb43bf08cb443e5aa797f4c161b629c4e4f2f42
coco-projects <coco-project@protonmail.com>
2026-01-27 17:36:23 +0800

    update

--- src/LauncherAbstract.php
+++ src/LauncherAbstract.php
@@ -4,68 +4,67 @@
 
     use Coco\logger\Logger;
 
-abstract class LauncherAbstract
-{
-    use Logger;
-
-    protected bool $isSudo = false;
-
-    public function __construct(protected string $command)
+    abstract class LauncherAbstract
     {
-    }
+        use Logger;
 
-    abstract public function launch(): void;
+        protected bool $isSudo = false;
 
-    abstract public function getLanuchCommand(): string;
+        public function __construct(protected string $command)
+        {
+        }
 
-    public function setIsSudo(bool $isSudo): static
-    {
-        $this->isSudo = $isSudo;
+        abstract public function launch(): void;
 
-        return $this;
-    }
+        abstract public function getLanuchCommand(): string;
 
-    protected function exec($command): array
-    {
-        exec($command, $output, $status);
+        public function setIsSudo(bool $isSudo): static
+        {
+            $this->isSudo = $isSudo;
 
-        if ($status === 0) {
-            $msg = "执行成功: " . $command;
-            $this->logInfo($msg);
+            return $this;
+        }
 
-            return [
+        protected function exec($command): array
+        {
+            exec($command, $output, $status);
+
+            $result = [
                 "output" => $output,
                 "status" => $status,
             ];
-        } else {
-            $msg = "执行失败: " . $command;
-            $msg .= json_encode($output, 256);
-            $this->logInfo($msg);
 
-            return [
-                "output" => $output,
-                "status" => $status,
+            if ($status === 0)
+            {
+                $msg = "执行成功: 【" . $command . '】【' . json_encode($result, 256) . '】';
+                $this->logInfo($msg);
+            }
+            else
+            {
+                $msg = "执行失败: 【" . $command . '】【' . json_encode($result, 256) . '】';
+                $this->logInfo($msg);
+            }
+
+            return $result;
+        }
+
+        protected static function formatMemorySize($size): string
+        {
+            $units = [
+                'B',
+                'KB',
+                'MB',
+                'GB',
+                'TB',
             ];
+
+            $i = 0;
+            while ($size >= 1024 && $i < count($units) - 1) {
+                $size /= 1024;
+                $i++;
+            }
+
+            // 格式化输出，保留两位小数
+            return round($size, 2) . ' ' . $units[$i];
         }
     }
-
-    protected static function formatMemorySize($size): string
-    {
-        $units = [
-            'B',
-            'KB',
-            'MB',
-            'GB',
-            'TB',
-        ];
-
-        $i = 0;
-        while ($size >= 1024 && $i < count($units) - 1) {
-            $size /= 1024;
-            $i++;
-        }
-
-        // 格式化输出，保留两位小数
-        return round($size, 2) . ' ' . $units[$i];
-    }
-}
